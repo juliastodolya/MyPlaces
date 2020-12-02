5e1c0f4742bdd3da6ba2b9c739389f9adcb4db51
Julia Stodolya <y.stodolya@gmail.com>
2020-12-02 11:37:27 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Юлия on 02.12.2020.
+//  Copyright © 2020 Julia Stodolya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
