c7f9f3f9a9fa24bfe90669fb621f4ca2fd41cf6f
Julia Stodolya <y.stodolya@gmail.com>
2020-12-02 19:36:05 +0300

    custom cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Юлия on 02.12.2020.
+//  Copyright © 2020 Julia Stodolya. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
