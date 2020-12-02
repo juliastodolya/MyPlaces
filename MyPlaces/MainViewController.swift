c1c41be5939c9b644db7873847cdd32f93a6464a
Julia Stodolya <y.stodolya@gmail.com>
2020-12-02 15:46:16 +0300

    images

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -33,7 +33,13 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        85
     }
     
 }
