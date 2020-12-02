2b9fceecc3437262cb35afebe41f6130be6125f7
Julia Stodolya <y.stodolya@gmail.com>
2020-12-02 15:04:37 +0300

    initial

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Юлия on 02.12.2020.
+//  Copyright © 2020 Julia Stodolya. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet var placesTableView: UITableView!
+    
+    let restaurantNames = ["Кансинь", "Фишка", "Корова", "Онегин" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        placesTableView.delegate = self
+        placesTableView.dataSource = self
+    }
+
+
+}
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell
+    }
+    
+}
